f1991219fb7c112761b58c4e2575b95443b80a61
frough <reihanifrough@gmai.com>
2024-07-28 03:04:40 +0330

    mr robot 5 has finished

--- shop/models.py
+++ shop/models.py
@@ -1,6 +1,7 @@
 import datetime
 
 from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
 import datetime
 class Category (models.Model):
     name = models.CharField(max_length=20)
@@ -24,6 +25,9 @@ class Product(models.Model):
     price = models.DecimalField(default=0, decimal_places=0, max_digits=12)
     category = models.ForeignKey(Category, on_delete=models.CASCADE, default=1)
     picture = models.ImageField(upload_to='upload/product/')
+    star = models.IntegerField(default=0, validators=[MaxValueValidator(5), MinValueValidator(3)])
+    is_sale = models.BooleanField(default=False)
+    sale_price = models.DecimalField(default=0, decimal_places=0, max_digits=12)
 
 def __str__(self):
     return self.name
