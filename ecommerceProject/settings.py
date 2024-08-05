2a06f56010b7444ff6a2fb8c628d02f8e1f42ede
frough <reihanifrough@gmai.com>
2024-08-06 00:00:19 +0330

    video 12 is completed

--- ecommerceProject/settings.py
+++ ecommerceProject/settings.py
@@ -55,7 +55,9 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
-            ],
+                'cart.context_processors.cart',
+
+],
         },
     },
 ]
